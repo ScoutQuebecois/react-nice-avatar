dbae89fa4a10c2bfd7c375c05b8ad4032cdec525
ScoutQuebecois <eloivl13@gmail.com>
2026-03-23 12:05:46 -0400

    EVL | tweaking sad feature

--- src/eyebrow/sad.tsx
+++ src/eyebrow/sad.tsx
@@ -14,8 +14,8 @@ export default function eyebrowSad(): SVGElement {
       viewBox="0 0 149 48"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
-      <path d="M99 20C104.667 22.6667 118 25.1 126 13.5" stroke="black" strokeWidth="4" strokeLinecap="round" />
-      <path d="M23.5791 5.521C25.6497 11.4316 33.2612 22.6461 47.1418 20.2196" stroke="black" strokeWidth="4" strokeLinecap="round" />
+      <path d="M99 25C104.667 27.6667 118 30.1 126 18.5" stroke="black" strokeWidth="4" strokeLinecap="round" />
+      <path d="M23.5791 40C25.6497 46 33.2612 47 47.1418 35" stroke="black" strokeWidth="4" strokeLinecap="round" />
     </svg>
   );
 }
