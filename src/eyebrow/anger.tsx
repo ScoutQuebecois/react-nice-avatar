16a9e5c89471912530f8b9f8ab86e60c5f0148a8
ScoutQuebecois <eloivl13@gmail.com>
2026-03-23 12:02:41 -0400

    EVL | tweaking anger feature

--- src/eyebrow/anger.tsx
+++ src/eyebrow/anger.tsx
@@ -14,8 +14,8 @@ export default function eyebrowAnger(): SVGElement {
       viewBox="0 0 149 48"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
-      <path d="M99 20C104.667 22.6667 118 25.1 126 13.5" stroke="black" strokeWidth="4" strokeLinecap="round" />
-      <path d="M23.5791 5.521C25.6497 11.4316 33.2612 22.6461 47.1418 20.2196" stroke="black" strokeWidth="4" strokeLinecap="round" />
+      <path d="M99 25C104 22 118 30 126 13.5" stroke="black" strokeWidth="4" strokeLinecap="round" />
+      <path d="M23 10C25 11 33 25 50 20.2196" stroke="black" strokeWidth="4" strokeLinecap="round" />
     </svg>
   );
 }
